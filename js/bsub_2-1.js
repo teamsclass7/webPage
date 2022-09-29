6b850962e14196dd9777a358697964d76551eb86
enldk <kimyw64@gmail.com>
2022-09-29 16:40:22 +0900

    뭐하냐고대체

--- js/bsub_2-1.js
+++ js/bsub_2-1.js
@@ -1,17 +1,19 @@
 var count = 1;
 var countEl = document.getElementById("count");
-var total_count = document.getElementById("total_count"); //추가
-var total_count_view = document.getElementById("total_count_view"); //추가
+var total_count = document.getElementById("total_count");
+var total_count_view = document.getElementById("total_count_view");
 function plus(){
   count++;
   countEl.value = count;
-  total_count_view.value = total_count.value * countEl.value; //추가
+  total_count_view.value = total_count.value * countEl.value;
 }
 function minus(){
   
   if (count > 1) {
     count--;
     countEl.value = count;
-  total_count_view.value = total_count_view.value - total_count.value; //추가  
+  total_count_view.value = total_count_view.value - total_count.value;
   }  
 }
+
+
