abc27d6bf00e9d267ee1f186892a07895c9ef17e
class7 <daram9148@gmail.com>
2022-09-29 17:35:23 +0900

    20220929_F

--- js/business.js
+++ js/business.js
@@ -1,23 +1,61 @@
-$(function(){
-   $(".main_menu>li").hover(function(){
-      $(this).children(".sub1").fadeIn(200);
-   },function(){
-      $(this).children(".sub1").fadeOut(200);
-   });
-});
+var wid = $(window).width();
+if(wid <= 1025){
+   $(".sub2>li").off("mouseover");
+   $(".sub1>li").off("mouseover");
+   $(".main_menu>li").off("mouseover");
+    var chk = true;
+   $(".main_menu>li").on("click", function(){
+      if(chk == true){
+         $(this).children(".sub1").fadeIn(200);
+      }else if(chk == false){
+         $(this).children(".sub1").fadeOut(200);
+      }
+      chk = !chk;
+    });
+    $(".sub1>li").on("click", function(){
+         if($(this).hasClass("sub2_chk")){
+            $(this).children(".sub2").fadeIn(200);
+            $(this).removeClass("sub2_chk");
+         }else {
+            $(this).children(".sub2").fadeOut(200);
+            $(this).addClass("sub2_chk");
+  
+      }
+      
+      chk = !chk;
+    });
+   }else {
+   sub1hover();
+   sub2hover();
+   
+}
 
-$(function(){
-   $(".sub1>li").hover(function(){
-      $(this).children(".sub2").fadeIn(200);
-   },function(){
-      $(this).children(".sub2").fadeOut(200);
-   });
+$(window).resize(function(){
+   location.reload(); // 윈도우 사이즈가 바뀔때 새로고침 해서 wid 변수 값을 변동 시킴. 
+   wid = $(window).width();
 });
+function sub2hover(){
+      $(".sub1>li").mouseover(function(){
+       $(this).children(".sub2").fadeIn(200);
+      });
+      $(".sub1>li").mouseleave(function(){
+       $(this).children(".sub2").fadeOut(200);
+      });
+}
+function sub1hover(){
+      $(".main_menu>li").mouseover(function(){
+       $(this).children(".sub1").fadeIn(200);
+      });
+      $(".main_menu>li").mouseleave(function(){
+       $(this).children(".sub1").fadeOut(200);
+      });
+}
 
+/*Mver*/
 $(function(){
    $("#menu").click(function(){
       $(this).toggleClass("aniMenu");
-      $(".main_menu").fadeToggle(100);
+      $(".main_menu").fadeToggle(300);
    });
 });
 
